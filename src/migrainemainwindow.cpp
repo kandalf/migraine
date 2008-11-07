7faab611b488201581e3dabdcd4cdc34ee7b06dd
Leonardo Mateo <lmateo@gisworking.com>
2008-11-07 09:11:06 -0200

    test for opendb

--- src/migrainemainwindow.cpp
+++ src/migrainemainwindow.cpp
@@ -1,4 +1,5 @@
 #include <QSqlDatabase>
+
 #include "migrainemainwindow.h"
 #include "connectiondialog.h"
 //
@@ -8,20 +9,31 @@ MigraineMainWindow::MigraineMainWindow( QWidget * parent, Qt::WFlags f)
 	setupUi(this);
 	dbSrcConnCombo->addItems(QSqlDatabase::connectionNames());
 	hSplitter->setStretchFactor(0,1);
+	
+	/*
+	QFile file("data/Database1.mdb");
+	if (file.open(QIODevice::ReadOnly)) {
+		logTextEdit->setText("Data file readable");
+		file.close();
+	} else {
+		logTextEdit->setText("Cannot open data file");
+		logTextEdit->append(file.errorString());
+	}*/
+		
 }
 
 void MigraineMainWindow::showConnectionDialog()
 {
-	ConnectionDialog *connDialog = new ConnectionDialog(this);
-	connDialog->show();
-	/*
+	/*ConnectionDialog *connDialog = new ConnectionDialog(this);
+	connDialog->show();*/
+	
 	QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-	db.setDatabaseName("DRIVER={Microsoft Access Driver (*.mdb)};FIL={MS Access};DBQ=database.mdb");
+	db.setDatabaseName("DRIVER={Microsoft Access Driver (*.mdb)};FIL={MS Access};DBQ=data/Database1.mdb");
 	db.open();
 	if (db.isOpen())
 		logTextEdit->setText("Open");
 	else
 		logTextEdit->setText("Canot open db");
-		*/
+		
 }
 //
